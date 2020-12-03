a7fd03348c4a1ae98554e4f2a4fc36c87bcea652
Neil M <n.mckenzie@live.com>
2020-12-03 13:51:20 -0600

    ok i hope this worked

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,6 @@ import LoginPage from "./Pages/loginpage";
 import RegisterPage from "./Pages/registerpage";
 import Home from "./Pages/Home";
 import IndexPage from "./Pages/indexpage";
-<<<<<<< HEAD
 import Chat from "./Pages/Chat";
 import Search from "./Pages/Search";
 import Settings from "./Pages/Settings";
@@ -14,9 +13,6 @@ import Footer from "./components/Footer/Footer";
 
 
 
-=======
-import TopicPage from "./Pages/topicpage";
->>>>>>> 3de96b78670c26f2bf4a45b41238485b862d3364
 
 function App() {
   return (
@@ -26,17 +22,11 @@ function App() {
         <Route path="/" component={IndexPage} exact />
         <Route path="/login" component={LoginPage} exact />
         <Route path="/register" component={RegisterPage} exact />
-<<<<<<< HEAD
         <Route path="/home" component={Home} exact />
         <Route path="/chat/:id" component={Chat} exact />
         <Route exact path="/search" component={Search}/>
         <Route exact path="/settings" component={Settings}/>
         <Route exact path="/chategorie" component={Chategorie}/>
-=======
-        <Route path="/dashboard" component={DashboardPage} exact />
-        <Route path="/topic/:id" component={TopicPage} exact />
-        
->>>>>>> 3de96b78670c26f2bf4a45b41238485b862d3364
       </Switch>
       <Footer />
     </BrowserRouter>
